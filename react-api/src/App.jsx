6159f9c6a1b54dfa74fa6513f7dc43ff9dda98df
Michel Lopes <lopesmichel.lp@gmail.com>
2025-04-11 23:57:30 -0300

    testando buscar dados

--- react-api/src/App.jsx
+++ react-api/src/App.jsx
@@ -4,8 +4,26 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
+   // 1 - resgatando dados
+  // useEffect(async () => {
+  //   const res = await fetch("http://localhost:3000/products");
+
+  //   const data = await res.json();
+
+  //   setProducts(data);
+  // }, []);
+
+  // 2 - add product
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const product = {
+      name,
+      price,
+    };
   return (
     <div className='App'>
+      
       <h1>Lista de produtos</h1>
     </div>
   )
